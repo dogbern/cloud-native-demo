20dd6106bb79ad4e480dc7539b6b9dceaef66b94
dogbern <dogbern@gmail.com>
2020-12-31 02:45:32 -0600

    Bug: fixed time update

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from flask import Flask, jsonify
-from datetime import datetime
+import time
 app = Flask(__name__)
-
-timestamp = int(datetime.now().timestamp())
-msg = {
-  'message': 'Automate all the things!',
-  'timestamp': timestamp
-}
 
 @app.route('/')
 def home():
   return '''
-  <h1> Demo API to showcase Cloud Native Experience</h1>
-  <p>version 1</p>
+  <h1>Demo API to showcase Cloud Native Experience - v1</h1>
   '''
 
-@app.route('/api/v1/msg')
+@app.route('/msg')
 def automate_msg():
+  timestamp = int(time.time())
+  msg = {
+    'message': 'Automate all the things!',
+    'timestamp': timestamp
+  }
   return jsonify(msg)
 
 if __name__ == '__main__':
